a3fa34f6ce3343e693099a4c4617ca9f5a3a5446
Danison dos Santos <danison.santos00@gmail.com>
2024-01-06 21:27:35 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HelloComponent } from './components/hello/hello.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HelloComponent
+    HelloComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,

9542abcf1a12e086cff032de61fb8e8f46e6b1df
Danison dos Santos <danison.santos00@gmail.com>
2024-01-11 20:59:55 -0300

    add projects

--- src/app/components/card-project/card-project.component.ts
+++ src/app/components/card-project/card-project.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'card-project',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class CardProjectComponent {
 
+  @Input() imgPath = ''
+  @Input() githubLink = '';
 }

5397425aef35bf886acd729f6a265f4f5d0fd005
Danison dos Santos <danison.santos00@gmail.com>
2024-01-11 23:40:07 -0300

    add animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portifolio';
+
+  constructor() {
+
+
+
+  }
+  ngOnInit(): void {
+    console.log("constructor");
+
+    const myObserver = new IntersectionObserver((entries) => {
+
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) entry.target.classList.add("show");
+        else entry.target.classList.remove("show");
+
+      })
+
+    });
+    const elements = document.querySelectorAll(".hidden");
+    elements.forEach((element) => {
+      console.log("foreach");
+
+      myObserver.observe(element);
+    })
+  }
+
 }
